bdb6395442d399a22564e26683da7ff44869594e
Eriksen Lezama <eriksen.lezama@gmail.com>
2022-07-16 21:54:54 -0500

    Base Chatview created to start conecting to server

--- src/components/ChatView/index.js
+++ src/components/ChatView/index.js
@@ -1,7 +1,57 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import css from './chatview.module.css'
 
 export const ChatView = () => {
+  const messageInput = useRef(null)
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    messageInput.current.focus()
+  }, [])
+
+  const enter = (e) => {
+    // if (e.keyCode === 13)
+    // sendMessage()
+  }
+
   return (
-    <div>ChatView</div>
+    <div className={css.container}>
+      <div className={css.usersContainer}>
+        <p className={css.usersTitle}>Connected Users</p>
+        <div className={css.listOfUsers}>
+          {
+            [0, 0, 0, 0].map((user, i) => (
+              <p key={i}>user</p>
+            ))
+          }
+        </div>
+      </div>
+      <div className={css.chatContainer}>
+        <div className={css.conversation}>
+          {
+            [0, 0, 0, 0].map((message, i) => (
+              <p key={i}>message</p>
+            ))
+          }
+        </div>
+        <div className={css.message}>
+          <input
+            ref={messageInput}
+            className={css.input}
+            id="message"
+            value={message}
+            onKeyUp={(e) => enter}
+            placeholder="Your message..."
+            onChange={(e) => setMessage(e.target.value)}
+            type="text"
+          />
+          <button
+            type='button'
+            className={css.button}
+            onClick={() => { }}
+          >&#9658;</button>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,13 +1,29 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useRef, useEffect } from 'react'
 import { WelcomeContext } from '@/contexts/WelcomeContext'
 import css from './welcome.module.css'
 
 export const Welcome = () => {
   const { state: { username }, setUsername, setShowChat } = useContext(WelcomeContext)
+  const [showAlert, setShowAlert] = useState(false)
+  const usernameInput = useRef(null)
 
   const join = () => {
-    setShowChat(true)
+    if (username !== '') {
+      setShowAlert(false)
+      setShowChat(true)
+    } else {
+      setShowAlert(true)
+    }
   }
+
+  const enter = (e) => {
+    if (e.keyCode === 13)
+      join()
+  }
+
+  useEffect(() => {
+    usernameInput.current.focus()
+  }, [])
 
   return (
     < div className={css.container}>
@@ -15,14 +31,18 @@ export const Welcome = () => {
       <label className={css.inputLabel} htmlFor="username">Username</label>
       <div className={css.inputContainer}>
         <input
+          ref={usernameInput}
           className={css.input}
           id="username"
           value={username}
+          onKeyUp={(e) => enter(e)}
+          placeholder="Your username..."
           onChange={(e) => setUsername(e.target.value)}
           type="text"
         />
-        <button className={css.button} onClick={join}>Join</button>
+        <button type='button' className={css.button} onClick={join}>Join</button>
       </div>
+      {showAlert && <p className={css.alert}>Username is required</p>}
     </div >
   )
 }
