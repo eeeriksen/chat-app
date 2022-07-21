0920fcf79fe88884f4f06903e37855203ec8e1a1
Eriksen Lezama <eriksen.lezama@gmail.com>
2022-07-20 20:16:31 -0500

    Each participant receiving conversation from each other

--- src/components/ChatConversation/ChatConversation.jsx
+++ src/components/ChatConversation/ChatConversation.jsx
@@ -5,14 +5,21 @@ import css from './chatConversation.module.css'
 
 export const ChatConversation = () => {
   const messageInput = useRef(null)
+  const conversationContainer = useRef(null)
+  const conversationElement = useRef(null)
+
   const [message, setMessage] = useState('')
+  const [conversation, setConversation] = useState([])
+  const [containerHeight, setContainerHeight] = useState(0)
+
   const { Socket } = useContext(SocketContext)
   const { state: { username } } = useContext(UsersContext)
-  const [conversation, setConversation] = useState([])
 
   const sendMessage = () => {
-    Socket.emit('sendMessage', { username, message, room: 'main' })
-    setMessage('')
+    if (message !== '') {
+      Socket.emit('sendMessage', { username, message, room: 'main' })
+      setMessage('')
+    }
   }
 
   const enter = (e) => {
@@ -29,24 +36,32 @@ export const ChatConversation = () => {
 
   useEffect(() => {
     messageInput.current.focus()
+
+    const observer = new ResizeObserver(entries => {
+      conversationContainer.current.scrollTo(0, conversationContainer.current.scrollHeight)
+    })
+    observer.observe(conversationElement.current)
   }, [])
 
   return (
     <div className={css.chatContainer}>
-      <div className={css.conversation}>
-        {
-          conversation.map((item, i) => (
-            <div className={`${username === item.username ? css.self : css.other} ${css.messageChat}`} key={i}>
-              <p className={css.usernameMessage}>{item.username}</p>
-              <p className={css.messageContent}>{item.message}</p>
-            </div>
-          ))
-        }
+      <div ref={conversationContainer} className={css.conversationContainer}>
+        <div ref={conversationElement} className={css.conversation}>
+          {
+            conversation.map((item, i) => (
+              <div className={`${username === item.username ? css.self : css.other} ${css.messageChat}`} key={i}>
+                <p className={css.usernameMessage}>{item.username}</p>
+                <p className={css.messageContent}>{item.message}</p>
+              </div>
+            ))
+          }
+        </div>
       </div>
       <div className={css.messageInput}>
         <input
           ref={messageInput}
           className={css.input}
+          autoComplete="off"
           id="message"
           value={message}
           onKeyUp={(e) => enter(e)}
